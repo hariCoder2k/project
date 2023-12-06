5462f633d74a0a26f77e8b5b98a6e3da8100c281
hariCoder2k <hariharakrishnan2002@gmail.com>
2023-12-06 11:34:58 +0530

    kaggled

--- data/datasets.py
+++ data/datasets.py
@@ -39,7 +39,7 @@ class BraTSDataset(Dataset):
     def __getitem__(self, index):
         path = self.paths[index].split('/')[-1]
         if self.list_file.split('/')[-2] == 'train':
-            save_path = "/kaggle/input/data-2/data2/BraTS_2018/pkg_files"
+            save_path = "/kaggle/input/data-2/data2/BraTS_2018/pkg_files/"
         if self.list_file.split('/')[-2] == 'valid':
             save_path = "./data/BraTS_2018/pkl_file/MICCAI_BraTS2018_ValidationData/"
         if self.list_file.split('/')[-2] == 'test':
